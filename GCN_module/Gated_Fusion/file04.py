07a88c2a5d2f16f0596747a2e3126c452d0f03a5
sellenzh <105904067+sellenzh@users.noreply.github.com>
2022-08-28 08:55:48 +0800

    Update file04.py

--- GCN_module/Gated_Fusion/file04.py
+++ GCN_module/Gated_Fusion/file04.py
@@ -7,7 +7,11 @@ class FC(nn.Module):
         #self.conv = nn.Conv2d(dims, dims, kernel_size=1, stride=1)
         self.linear = nn.Linear(dims, dims)#way1
         '''self.layer1 = nn.Linear(dims, self.hidden)
-        self.layer2 = nn.Linear(self.hidden, dims)'''#way2
+        self.layer2 = nn.Linear(self.hidden, self.hidden)
+        self.layer3 = nn.Linear(self.hidden, dims)
+        self.relu1 = nn.ReLU()
+        self.relu2 = nn.ReLU()
+        self.relu3 = nn.ReLU()'''#way2
         self.bn = nn.BatchNorm2d(dims)
         self.act = activation if activation is not None else nn.ReLU()
         self.dropout = nn.Dropout(p=dropout) if dropout is not None else nn.Identity()
@@ -17,7 +21,10 @@ class FC(nn.Module):
         x = self.linear(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)#way1
 
         '''x = self.layer1(x.permute(0, 2, 3, 1))
-        x = self.layer2(x).permute(0, 3, 1, 2)'''
+        x = self.relu1(x)
+        x = self.relu2(self.layer2(x))
+        x = self.layer3(x).permute(0, 3, 1, 2)
+        x = self.relu3(x)'''
         y = self.act(self.bn(x))
         y = self.dropout(y)
         return y
