f6683c54d8088bf06251d02bfe3d3e8664b973b2
sellenzh <105904067+sellenzh@users.noreply.github.com>
2022-08-28 11:28:30 +0800

    Update file05.py

--- GCN_module/Gated_Fusion/file05.py
+++ GCN_module/Gated_Fusion/file05.py
@@ -4,28 +4,31 @@ from torch import nn
 class FC(nn.Module):
     def __init__(self, dims, activation=None, dropout=None):
         super(FC, self).__init__()
-        self.hidden = dims * 2
+        self.hidden = dims * 3
         #self.conv = nn.Conv2d(dims, dims, kernel_size=1, stride=1)
-        self.linear = nn.Linear(dims, dims)#way1
-        '''self.layer1 = nn.Linear(dims, self.hidden)
+        #self.linear = nn.Linear(dims, dims)#way1
+        self.layer1 = nn.Linear(dims, self.hidden)
         self.layer2 = nn.Linear(self.hidden, self.hidden)
-        self.layer3 = nn.Linear(self.hidden, dims)
+        self.layer3 = nn.Linear(self.hidden, self.hidden)
+        self.layer4 = nn.Linear(self.hidden, dims)
         self.relu1 = nn.ReLU()
         self.relu2 = nn.ReLU()
-        self.relu3 = nn.ReLU()'''#way2
+        self.relu3 = nn.ReLU()
+        self.relu4 = nn.ReLU()#way2
         self.bn = nn.BatchNorm2d(dims)
         self.act = activation if activation is not None else nn.ReLU()
         self.dropout = nn.Dropout(p=dropout) if dropout is not None else nn.Identity()
 
     def forward(self, x):
         #x = self.conv(x)
-        x = self.linear(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)#way1
+        #x = self.linear(x.permute(0, 2, 3, 1)).permute(0, 3, 1, 2)#way1
 
-        '''x = self.layer1(x.permute(0, 2, 3, 1))
+        x = self.layer1(x.permute(0, 2, 3, 1))
         x = self.relu1(x)
         x = self.relu2(self.layer2(x))
-        x = self.layer3(x).permute(0, 3, 1, 2)
-        x = self.relu3(x)'''
+        x = self.relu3(self.layer3(x))
+        x = self.layer4(x).permute(0, 3, 1, 2)
+        x = self.relu4(x)
         y = self.act(self.bn(x))
         y = self.dropout(y)
         return y
