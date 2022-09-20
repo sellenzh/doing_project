a2ce8b989d38d55dabad333e9c66f829cbc26fd5
sellenzh <105904067+sellenzh@users.noreply.github.com>
2022-09-20 21:33:15 +0800

    Create data_interface.py

---
+++ Project/data/data_interface.py
@@ -0,0 +1,61 @@
+import inspect
+import importlib
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+class DInterface(pl.LightningDataModule):
+    def __init__(self, num_workers=8, dataset='', **kwargs):
+        super().__init__()
+        self.num_workers = num_workers
+        self.dataset = dataset
+        self.kwargs = kwargs
+        self.batch_size = kwargs['batch_size']
+        self.load_data_module()
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == 'fit' or stage is None:
+            self.trainset = self.instancialize(train=True)
+            self.valset = self.instancialize(train=False)
+        # Assign test dataset for use in dataloader(s)
+        if stage == 'test' or stage is None:
+            self.testset = self.instancialize(train=False)
+
+    def train_dataloader(self):
+        return  DataLoader(self.trainset,
+                           batch_size=self.batch_size,
+                           num_workers=self.num_workers,
+                           shuffle=True)
+
+    def val_dataloader(self):
+        return  DataLoader(self.valset,
+                           batch_size=self.batch_size,
+                           num_workers=self.num_workers,
+                           shuffle=False)
+
+    def test_dataloader(self):
+        return  DataLoader(self.testset,
+                           batch_size=self.batch_size,
+                           num_workers=self.num_workers,
+                           shuffle=False)
+
+    def load_data_module(self):
+        name = self.dataset
+        camel_name = ''.join([i.capitalize() for i in name.split('_')])
+        try:
+            self.data_module = getattr(importlib.import_module(
+                '.' + name, package=__package__), camel_name)
+        except:
+            raise ValueError(
+                f'Invalid Dataset File Name or Invalid Class Name data.{name}.{camel_name}'
+            )
+
+    def instancialize(self, **other_args):
+        class_args = inspect.getfullargspec(self.data_module.__init__).args[1:]
+        inkeys = self.kwargs.keys()
+        args1 = {}
+        for arg in class_args:
+            if arg in inkeys:
+                args1[arg] = self.kwargs[arg]
+        args1.update(other_args)
+        return self.data_module(**args1)
