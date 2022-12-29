4b685c35e2e557bbc4b12a13fc7a66eac90f3659
sellenzh <105904067+sellenzh@users.noreply.github.com>
2022-12-29 19:12:21 +0800

    Update add_bbox2pkl.py

--- pre_data/add_bbox2pkl.py
+++ pre_data/add_bbox2pkl.py
@@ -4,9 +4,9 @@ import os
 
 np.set_printoptions(threshold=np.inf)
 
-def load_xml_coordinates(path, file, pid, fid):
+def load_xml_coordinates(path, file, pid, fid, i):
     #print(file)
-    vid = file.split('_')[1][:4]
+    #vid = file.split('_')[1][:4]
     #print(vid)
     file = path + file
     with open(file, 'rb') as f:
@@ -16,12 +16,15 @@ def load_xml_coordinates(path, file, pid, fid):
         if p != pid:
             continue
         for fr in ped.split('<box frame="')[1:]: #每帧数据：...<box frame=*xxx*" keyframe=...
-            if int(fr.split('" keyframe')[0]) == int(fid):
-                return fr.split('xbr="')[1].split('"')[0], fr.split('xtl="')[1].split('"')[0], fr.split('ybr="')[1].split('"')[0], fr.split('ytl="')[1].split('"')[0]
+            if int(fr.split('" keyframe')[0]) == int(fid) + i:
+                if fr.split('xbr="')[1].split('"')[0] is not None and fr.split('xtl="')[1].split('"')[0] is not None and fr.split('ybr="')[1].split('"')[0] is not None and fr.split('ytl="')[1].split('"')[0] is not None:
+                    return fr.split('xbr="')[1].split('"')[0], fr.split('xtl="')[1].split('"')[0], fr.split('ybr="')[1].split('"')[0], fr.split('ytl="')[1].split('"')[0]
+            else:
+                return '0.0', '0.0', '0.0', '0.0'
 
-def readbbox(vid, pid, fid):
+def readbbox(vid, pid, fid, i):
     file_name = 'video_' + vid + '.xml'
-    return load_xml_coordinates(jaad_annotations_path, file_name, pid, fid)
+    return load_xml_coordinates(jaad_annotations_path, file_name, pid, fid, i)
     #return xbr, xtl, ybr, ytl
     
 def load_write_pkl(path, file):
@@ -34,11 +37,16 @@ def load_write_pkl(path, file):
     fid = str(file.split('_')[-1].split('.')[0])
     #print(fid)
     filename = path + file
-    xbr, xtl, ybr, ytl = readbbox(vid, pid, fid)
+    coordinates = []
+    for i in range(32):
+        '''if i == 29:
+            print('ok.')'''
+        xbr, xtl, ybr, ytl = readbbox(vid, pid, fid, i)
+        coordinates.append([float(xbr), float(xtl), float(ybr), float(ytl)])
     with open(filename, 'rb') as f:
         data = pk.load(f)
         #print(type(data)) # data's type is: dict -> {key: values, ...}
-    data['bbox'] = [xbr, xtl, ybr, ytl]
+    data['bbox'] = coordinates
     with open(filename, 'wb') as f:
         pk.dump(data, f)
     #print(str(vid) + '_' + str(pid) + '_' + str(fid) + 'Finished!')
@@ -50,12 +58,17 @@ kps_path = os.getcwd() + '/data/JAAD/data/'
 #kps_path = os.getcwd() + '/test/pkl/'
 kps = os.walk(kps_path)
 count = 0
+#flag = False
 for path, dir_list, file_list in kps:
     for file in file_list:
         #print(file) #get file as: video_0001.xml
         #load_xml(path, file)
+        #if flag:
+        #print(file)
         load_write_pkl(path, file)
         count += 1
         if (count % 100 == 0):
             print(str(count) + ' files finished.')
+            '''if count == 11300:
+                flag = True'''
 print('   ' + str(count) + ' files finished!')
