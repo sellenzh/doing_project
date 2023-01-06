5cca219e244a6df6dda1076ee66b522402e7c606
sellenzh <105904067+sellenzh@users.noreply.github.com>
2023-01-06 15:35:54 +0800

    Update add_bbox2pkl.py

--- pre_data/add_bbox2pkl.py
+++ pre_data/add_bbox2pkl.py
@@ -19,20 +19,34 @@ def load_xml_coordinates(path, file, pid, fid, i):
             if int(fr.split('" keyframe')[0]) == int(fid) + i:
                 if fr.split('xbr="')[1].split('"')[0] is not None and fr.split('xtl="')[1].split('"')[0] is not None and fr.split('ybr="')[1].split('"')[0] is not None and fr.split('ytl="')[1].split('"')[0] is not None:
                     return fr.split('xbr="')[1].split('"')[0], fr.split('xtl="')[1].split('"')[0], fr.split('ybr="')[1].split('"')[0], fr.split('ytl="')[1].split('"')[0]
-            else:
-                return '0.0', '0.0', '0.0', '0.0'
+        return '0.0', '0.0', '0.0', '0.0'
 
 def readbbox(vid, pid, fid, i):
     file_name = 'video_' + vid + '.xml'
     return load_xml_coordinates(jaad_annotations_path, file_name, pid, fid, i)
     #return xbr, xtl, ybr, ytl
+
+def load_crosspoint(vid, pid):
+    file_name_ori = 'video_' + vid + '.xml'
+    file_name = 'video_' + vid + '_attributes.xml'
+    file = jaad_attributes_path + file_name
+    with open(file, 'rb') as f:
+        data = str(f.read())
+    for ped in data.split('crossing_point="')[1:]:
+        if pid == ped.split('id="')[1].split('" intersection=')[0]:
+            cross_point = ped.split('" decision_point=')[0]
+            if cross_point == '-1':
+                break
+            return load_xml_coordinates(jaad_annotations_path, file_name_ori, pid, cross_point, 0)
+    return '0.0', '0.0', '0.0', '0.0'
     
 def load_write_pkl(path, file):
     #print(path)
     #print(file)
-    vid = str(file.split('_')[1])
+    sid = str(file.split('_')[0][-2:])
+    vid = str(file.split('_')[2])
     #print(vid)
-    pid = str(file.split('_')[3]) + '_' + str(file.split('_')[4]) + '_' + str(file.split('_')[5])
+    pid = str(file.split('_')[4]) + '_' + str(file.split('_')[5]) + '_' + str(file.split('_')[6])
     #print(pid)
     fid = str(file.split('_')[-1].split('.')[0])
     #print(fid)
@@ -47,14 +61,17 @@ def load_write_pkl(path, file):
         data = pk.load(f)
         #print(type(data)) # data's type is: dict -> {key: values, ...}
     data['bbox'] = coordinates
+    xbr, xtl, ybr, ytl = load_crosspoint(vid, pid)
+    data['cross_point'] = [float(xbr), float(xtl), float(ybr), float(ytl)]
     with open(filename, 'wb') as f:
         pk.dump(data, f)
     #print(str(vid) + '_' + str(pid) + '_' + str(fid) + 'Finished!')
 
-jaad_annotations_path = os.getcwd() + '/JAAD/annotations/'
+jaad_annotations_path = os.getcwd() + '/PIE/annotations/'
 #jaad_annotations_path = os.getcwd() + '/test/xml/'
-annot = os.walk(jaad_annotations_path)
-kps_path = os.getcwd() + '/data/JAAD/data/'
+#annot = os.walk(jaad_annotations_path)
+jaad_attributes_path = os.getcwd() + '/PIE/annotations_attributes/'
+kps_path = os.getcwd() + '/PIE_dataset/data/'
 #kps_path = os.getcwd() + '/test/pkl/'
 kps = os.walk(kps_path)
 count = 0
